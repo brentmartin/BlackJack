75680121519bb733503e98a0d6c399b2218efee7
Brent Martin <bmart35@gmail.com>
2016-03-10 09:00:13 -0600

    Initialize files

---
+++ bin/blackjack.rb
@@ -0,0 +1,131 @@
+def deal
+  @cards = []
+  @p_hand = []
+  @c_hand = []
+  4.times do
+    (1..13).each do |n|
+      @cards.push(n)
+    end
+  end
+
+  def cards_remaining
+    @cards.length
+  end
+
+  def shuffle
+    @cards = @cards.shuffle
+  end
+
+  def draw
+    @cards.shift
+  end
+
+  @deck = shuffle
+  # puts @cards
+  # puts "*" * 60
+  # puts cards_remaining
+  # puts "*" * 60
+  puts @deck
+  puts "*" * 60
+  # puts draw
+end
+
+def play
+  deal
+  p_draw
+  p_turn
+  c_draw
+  c_turn
+  p_showhand_UI
+  c_showhand_UI
+end
+
+def p_showhand_UI
+  system('clear')
+  puts "PLAYER hand"
+  print @p_hand
+  puts
+  puts
+  puts "total is *#{p_sum}*"
+  puts
+  puts "-" * 60
+end
+
+def c_showhand_UI
+  puts "DEALER hand"
+  print @c_hand
+  puts
+  puts
+  puts "total is *#{c_sum}*"
+  puts
+  puts "-" * 60
+end
+
+def c_thinking_UI
+  print "."
+  sleep 1
+  print "."
+  sleep 1
+  print "."
+  sleep 1
+end
+
+
+def p_draw
+  2.times do
+    @p_hand.push @deck.shift
+  end
+end
+
+def c_draw
+  2.times do
+    @c_hand.push @deck.shift
+  end
+end
+
+def p_turn
+  p_hit = "Y"
+  while p_hit.upcase == "Y"
+    p_showhand_UI
+    puts "Would you like another hit? (y) or (n)"
+    print "> "
+    p_hit = gets.chomp
+    if p_hit.upcase ==  "Y"
+      @p_hand.push @deck.shift
+    else
+    end
+  end
+end
+
+def c_turn
+  p_showhand_UI
+  c_showhand_UI
+  puts "Dealer take another hit?"
+  print "> "
+  c_thinking_UI
+  if c_sum <= 16
+    @c_hand.push @deck.shift
+    c_turn
+  end
+end
+
+def p_sum
+  p_total = 0
+  @p_hand.each do | card |
+    p_total = card + p_total
+  end
+  p_total
+end
+
+def c_sum
+  c_total = 0
+  @c_hand.each do | card |
+    c_total = card + c_total
+  end
+  c_total
+end
+
+
+
+
+play

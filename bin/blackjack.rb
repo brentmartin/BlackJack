f59c9bbb9ec66fe673a2c8ce835b05829d527362
Brent Martin <bmart35@gmail.com>
2016-06-04 23:27:41 -0500

    added a new intro

--- bin/blackjack.rb
+++ bin/blackjack.rb
@@ -2,7 +2,36 @@ require_relative '../lib/deck'
 require_relative '../lib/player'
 require_relative '../lib/dealer'
 
+def intro
+  system('clear')
+  puts "Welcome to Blackjack, what is your name?"
+  print "> "
+  name = gets.chomp
+  sleep 1
+  system('clear')
+  print "Howdy #{name}, "
+end
 
+def challenge
+  puts "shall we play? (y) or (n)"
+  print "> "
+  question_playgame = gets.chomp
+  if question_playgame == "y"
+    system('clear')
+    puts "Alright! Lets get started."
+    sleep 2
+    play
+  elsif question_playgame == "n"
+    system('clear')
+    puts "Have a wonderful evening!"
+    sleep 2
+  else
+    system('clear')
+    puts "I'm sorry, I didn't understand your answer."
+    sleep 2
+    system('clear')
+    print "So "
+    challenge
   end
 end
 
@@ -100,5 +129,5 @@ end
 
 
 
-
-play
+intro
+challenge

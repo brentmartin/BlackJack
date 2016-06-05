7af0355934f61fc3ae79860dd0ac9c796f55571a
Brent Martin <bmart35@gmail.com>
2016-06-04 23:35:56 -0500

    filled out dealer class

--- lib/dealer.rb
+++ lib/dealer.rb
@@ -1,4 +1,15 @@
 class Dealer
+  attr_accessor :c_score, :c_hand
   def initialize
+    @c_score = c_score
+    @c_hand = c_hand
+  end
+
+  def hand
+    @c_hand = []
+  end
+
+  def score
+    @c_score = 0
   end
 end

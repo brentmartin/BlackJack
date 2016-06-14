0a3cf1e53a1b03e214c1cea13ffaa30be48fa1b7
Jack-Barry <sirnapier88@gmail.com>
2016-06-13 23:48:38 -0500

    Reconfigure folders within lib

--- bin/blackjack.rb
+++ bin/blackjack.rb
@@ -2,12 +2,4 @@
 current_folder = File.expand_path('../../', __FILE__)
 Dir["#{current_folder}/**/*.rb"].each { |f| require f }
 
-def boot_up
-  intro_name
-  name      = gets.chomp
-  @player   = Player.new(name)
-  @computer = Player.new(nil)
-  intro_challenge
-end
-
 boot_up
